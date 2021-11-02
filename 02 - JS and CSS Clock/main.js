048d892ea81b7be2f9381841c61b8f771a80f531
Christian BeauxYeux <gildweek@gmail.com>
2021-11-02 11:30:52 +0400

    Added dark mode style

--- 02 - JS and CSS Clock/main.js
+++ 02 - JS and CSS Clock/main.js
@@ -8,7 +8,14 @@ const setDate = () => {
   const mins = now.getMinutes();
   const minsDeg = (( mins / 60) * 360) + 90;
   const minH = document.querySelector('.min-hand');
-  minH = style.transform = `rotate(${minsDeg}deg)`;
+  minH.style.transform = `rotate(${minsDeg}deg)`;
+
+  const hours = now.getHours();
+  const hoursDeg = (( hours / 12 ) * 360) + 90;
+  const hourH = document.querySelector('.hour-hand');
+  hourH.style.transform = `rotate(${hoursDeg}deg)`;
+
+
 }
 
 setInterval(setDate, 1000);
