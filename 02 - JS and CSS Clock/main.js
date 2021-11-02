2d839e2874ca66d583e38a691a105ba04939494a
Christian BeauxYeux <gildweek@gmail.com>
2021-11-02 11:08:37 +0400

    Added minutes hand logic to function

--- 02 - JS and CSS Clock/main.js
+++ 02 - JS and CSS Clock/main.js
@@ -1,9 +1,14 @@
 const setDate = () => {
   const now = new Date();
   const secs = now.getSeconds();
-  const secsDeg = (( secs / 60 ) * 360);
+  const secsDeg = (( secs / 60 ) * 360) + 90;
   const secH = document.querySelector('.second-hand');
   secH.style.transform = `rotate(${secsDeg}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDeg = (( mins / 60) * 360) + 90;
+  const minH = document.querySelector('.min-hand');
+  minH = style.transform = `rotate(${minsDeg}deg)`;
 }
 
 setInterval(setDate, 1000);
